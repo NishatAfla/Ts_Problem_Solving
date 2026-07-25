2d59aa00a155c9f45d6ac7e55c275eb0145657b2
NishatAfla <nishatafla21@gmail.com>
2026-07-26 00:30:08 +0600

    Ignore generated JavaScript files

--- qs7.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Print Unique Characters from a String
-Object.defineProperty(exports, "__esModule", { value: true });
-const text = "abcaabbcc";
-// Remove duplicate characters
-const uniqueCharacters = [...new Set(text)];
-console.log("Unique characters:", uniqueCharacters.join(", "));
-console.log("Total unique characters:", uniqueCharacters.length);
-//# sourceMappingURL=qs7.js.map
